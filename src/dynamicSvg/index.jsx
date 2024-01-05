b484c39c909791015e021d2dca5da1cb263ad265
umer abbas <umerabbas@codexsquared.com>
2024-01-05 20:02:57 +0500

    variables added, basic nodes array created with initial objects

--- src/dynamicSvg/index.jsx
+++ src/dynamicSvg/index.jsx
@@ -1,9 +1,35 @@
 import LockIcon from './assets/icons8-lock.svg';
+
+const lightOrange = '#fbd5b9';
+const darkOrange = '#f16020';
+const gray = '#b9b9b9';
+
+const patternHeight = 2;
+const patternWidth = 400;
+
+const strokeWidth = 3
+
+let cx = 35;
+let cy = 35;
+const xGap = 350;
+const yGap = 350;
+const radius = 20;
+
+const nodesArr = [
+  {
+    id: '1',
+    icon: null,
+    isCurrent: true,
+    isComplete: false,
+  },
+  {
+    id: '2',
+    icon: LockIcon,
+    isComplete: false,
+  }
+];
+
 function DynamicSVG() {
-  // lock color #888888
-  // gray color #b9b9b9
-  // darkOrange #f16020
-  // lightOrange #fbd5b9
 
   const handleClick = () => {
     const radius = 20;
@@ -40,37 +66,63 @@ function DynamicSVG() {
 
   return (
     <>
-      <button onClick={handleClick}>Click</button>
-      <svg width="800" height="400">
+      <button onClick={handleClick} style={{ position: "absolute", top: "0", left: "400px" }}>Click</button>
+      <svg width="800" height="1000">
         <defs>
-          <linearGradient id="gradient">
-            <stop offset="0%" stopColor="#f16020" />
-            <stop offset="100%" stopColor="#b9b9b9" />
+          <linearGradient id="completePathGradient">
+            <stop offset="0%" stopColor={darkOrange} />
           </linearGradient>
-          <pattern id="pathPattern" x="0" y="0" width="400" height="2" patternUnits="userSpaceOnUse">
+          <linearGradient id="currentPathGradient">
+            <stop offset="0%" stopColor={darkOrange} />
+            <stop offset="100%" stopColor={gray} />
+          </linearGradient>
+          <linearGradient id="remainingPathGradient">
+            <stop offset="0%" stopColor={gray} />
+          </linearGradient>
+
+          <pattern id="completePathPattern" x="0" y="0" width={patternWidth} height={patternHeight} patternUnits="userSpaceOnUse">
             <g>
-              <rect x="0" y="0" width="400" height="2" fill="url(#gradient)" />
+              <rect x="0" y="0" width={patternWidth} height={patternHeight} fill="url(#completePathGradient)" />
             </g>
           </pattern>
-
+          <pattern id="currentPathPattern" x="0" y="0" width={patternWidth} height={patternHeight} patternUnits="userSpaceOnUse">
+            <g>
+              <rect x="0" y="0" width={patternWidth} height={patternHeight} fill="url(#currentPathGradient)" />
+            </g>
+          </pattern>
+          <pattern id="remainingPathPattern" x="0" y="0" width={patternWidth} height={patternHeight} patternUnits="userSpaceOnUse">
+            <g>
+              <rect x="0" y="0" width={patternWidth} height={patternHeight} fill="url(#remainingPathGradient)" />
+            </g>
+          </pattern>
           <pattern id="lockImage" x="0%" y="0%" height="100%" width="100%" viewBox="0 0 512 512">
             <image x="5%" y="5%" width="430" height="430" xlinkHref={LockIcon} />
           </pattern>
         </defs>
 
+        <circle id="au" cx={cx} cy={cy} r="35" fill={lightOrange} />
+        <circle id="au1" cx={cx} cy={cy} r="22" fill={darkOrange} />
 
+        <circle id="sl" cx="370" cy="200" r="22" fill={gray} />
+        <circle id="sl1" cx="370" cy="200" r="18" fill="url(#lockImage)" />
 
+        <circle id="dot" cx="0" cy="0" r="2" stroke="green" strokeWidth={strokeWidth} fill="red" />
 
-        <circle id="au" cx="35" cy="35" r="35" strokeWidth="3" fill="#fbd4b8" />
-        <circle id="au" cx="35" cy="35" r="22" strokeWidth="3" fill="#f16020" />
+        <path 
+          id='curve' 
+          style={{ stroke: 'url(#currentPathPattern)' }} 
+          fill='transparent' 
+          strokeWidth='4' 
+          strokeDasharray="13,10"
+          d='M35,50 Q40,100 150,50 T365,170' 
+        />
 
-        <circle id="sl" cx="370" cy="200" r="22" strokeWidth="3" fill="#b9b9b9" />
-        <circle id="sl" cx="370" cy="200" r="18" strokeWidth="3" fill="url(#lockImage)" />
+        {nodesArr.map(node => {
 
-        <circle id="dot" cx="0" cy="0" r="2" stroke="green" strokeWidth="3" fill="red" />
-
-        <path id='curve' style={{ stroke: 'url(#pathPattern)' }} fill='transparent' strokeWidth='4' strokeDasharray="13,10"
-          d='M35,50 Q40,100 150,50 T365,170' />
+          return (<>
+            
+          </>);
+        })}
       </svg>
     </>
   )

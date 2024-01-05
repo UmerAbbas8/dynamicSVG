c6d19dd6c70ee537c592646f5147c3ccd14cc98f
umer abbas <umerabbas@codexsquared.com>
2024-01-05 21:05:56 +0500

    loop fixed

--- src/dynamicSvg/index.jsx
+++ src/dynamicSvg/index.jsx
@@ -1,4 +1,7 @@
+import { useRef } from 'react';
 import LockIcon from './assets/icons8-lock.svg';
+import OkIcon from './assets/icons8-ok.svg';
+import SettingIcon from './assets/icons8-setting.svg';
 
 const lightOrange = '#fbd5b9';
 const darkOrange = '#f16020';
@@ -7,23 +10,34 @@ const gray = '#b9b9b9';
 const patternHeight = 2;
 const patternWidth = 400;
 
-const strokeWidth = 3
-
-let cx = 35;
-let cy = 35;
-const xGap = 350;
-const yGap = 350;
-const radius = 20;
+const cxInit = 35;
+const xGap = 300;
+const yGap = 185;
 
 const nodesArr = [
   {
     id: '1',
     icon: null,
+    isComplete: true,
+  },
+  {
+    id: '2',
+    icon: null,
     isCurrent: true,
     isComplete: false,
   },
   {
-    id: '2',
+    id: '3',
+    icon: LockIcon,
+    isComplete: false,
+  },
+  {
+    id: '4',
+    icon: LockIcon,
+    isComplete: false,
+  },
+  {
+    id: '5',
     icon: LockIcon,
     isComplete: false,
   }
@@ -31,100 +45,128 @@ const nodesArr = [
 
 function DynamicSVG() {
 
-  const handleClick = () => {
-    const radius = 20;
-    var p1x = parseFloat(document.getElementById("au").getAttribute("cx"));
-    var p1y = parseFloat(document.getElementById("au").getAttribute("cy"));
-    var p2x = parseFloat(document.getElementById("sl").getAttribute("cx"));
-    var p2y = parseFloat(document.getElementById("sl").getAttribute("cy"));
+  const nodesRef = useRef([]);
 
-    // mid-point of line:
-    var mpx = (p2x + p1x + radius) * 0.6;
-    var mpy = (p2y + p1y + radius) * 0.5;
-
-    // angle of perpendicular to line:
-    var theta = Math.atan2(p2y - p1y, p2x - p1x) - Math.PI / 2;
-
-    // distance of control point from mid-point of line:
-    var offset = 60;
-
-    // location of control point:
-    var c1x = mpx + offset * Math.cos(theta);
-    var c1y = mpy + offset * Math.sin(theta);
-    var dotElement = document.getElementById("dot");
-    dotElement.setAttribute("cx", c1x);
-    dotElement.setAttribute("cy", c1y);
-
-    const pathOffset = radius * 1.45;
-
-    // construct the command to draw a quadratic curve
-    // var curve = "M" + p1x + " " + p1y + " Q " + c1x + " " + c1y + " " + p2x + " " + p2y;
-    var curve = "M" + (p1x + pathOffset) + " " + p1y + " Q " + c1x + " " + c1y + " " + p2x + " " + (p2y - pathOffset);
-    var curveElement = document.getElementById("curve");
-    curveElement.setAttribute("d", curve);
-  }
+  let cx = 35;
+  let cy = 35;
 
   return (
-    <>
-      <button onClick={handleClick} style={{ position: "absolute", top: "0", left: "400px" }}>Click</button>
-      <svg width="800" height="1000">
-        <defs>
-          <linearGradient id="completePathGradient">
-            <stop offset="0%" stopColor={darkOrange} />
-          </linearGradient>
-          <linearGradient id="currentPathGradient">
-            <stop offset="0%" stopColor={darkOrange} />
-            <stop offset="100%" stopColor={gray} />
-          </linearGradient>
-          <linearGradient id="remainingPathGradient">
-            <stop offset="0%" stopColor={gray} />
-          </linearGradient>
+    <svg width="800" height="1000">
+      <defs>
+        <linearGradient id="completePathGradient">
+          <stop offset="0%" stopColor={darkOrange} />
+        </linearGradient>
+        <linearGradient id="currentPathGradient">
+          <stop offset="0%" stopColor={darkOrange} />
+          <stop offset="100%" stopColor={gray} />
+        </linearGradient>
+        <linearGradient id="remainingPathGradient">
+          <stop offset="0%" stopColor={gray} />
+        </linearGradient>
 
-          <pattern id="completePathPattern" x="0" y="0" width={patternWidth} height={patternHeight} patternUnits="userSpaceOnUse">
-            <g>
-              <rect x="0" y="0" width={patternWidth} height={patternHeight} fill="url(#completePathGradient)" />
-            </g>
-          </pattern>
-          <pattern id="currentPathPattern" x="0" y="0" width={patternWidth} height={patternHeight} patternUnits="userSpaceOnUse">
-            <g>
-              <rect x="0" y="0" width={patternWidth} height={patternHeight} fill="url(#currentPathGradient)" />
-            </g>
-          </pattern>
-          <pattern id="remainingPathPattern" x="0" y="0" width={patternWidth} height={patternHeight} patternUnits="userSpaceOnUse">
-            <g>
-              <rect x="0" y="0" width={patternWidth} height={patternHeight} fill="url(#remainingPathGradient)" />
-            </g>
-          </pattern>
-          <pattern id="lockImage" x="0%" y="0%" height="100%" width="100%" viewBox="0 0 512 512">
-            <image x="5%" y="5%" width="430" height="430" xlinkHref={LockIcon} />
-          </pattern>
-        </defs>
+        <pattern id="completePathPattern" x="0" y="0" width={patternWidth} height={patternHeight} patternUnits="userSpaceOnUse">
+          <g>
+            <rect x="0" y="0" width={patternWidth} height={patternHeight} fill="url(#completePathGradient)" />
+          </g>
+        </pattern>
+        <pattern id="currentPathPattern" x="0" y="0" width={patternWidth} height={patternHeight} patternUnits="userSpaceOnUse">
+          <g>
+            <rect x="0" y="0" width={patternWidth} height={patternHeight} fill="url(#currentPathGradient)" />
+          </g>
+        </pattern>
+        <pattern id="remainingPathPattern" x="0" y="0" width={patternWidth} height={patternHeight} patternUnits="userSpaceOnUse">
+          <g>
+            <rect x="0" y="0" width={patternWidth} height={patternHeight} fill="url(#remainingPathGradient)" />
+          </g>
+        </pattern>
+        <pattern id="lockImage" x="0%" y="0%" height="100%" width="100%" viewBox="0 0 512 512">
+          <image x="5%" y="5%" width="430" height="430" xlinkHref={LockIcon} />
+        </pattern>
+        <pattern id="okImage" x="0%" y="0%" height="100%" width="100%" viewBox="0 0 512 512">
+          <image x="5%" y="5%" width="430" height="430" xlinkHref={OkIcon} />
+        </pattern>
+        <pattern id="settingImage" x="0%" y="0%" height="100%" width="100%" viewBox="0 0 512 512">
+          <image x="5%" y="5%" width="430" height="430" xlinkHref={SettingIcon} />
+        </pattern>
+      </defs>
 
-        <circle id="au" cx={cx} cy={cy} r="35" fill={lightOrange} />
-        <circle id="au1" cx={cx} cy={cy} r="22" fill={darkOrange} />
+      {nodesArr.map((node, idx) => {
+        if (idx > 0) {
+          cy += yGap;
+        }
+        if (idx % 2 === 0) {
+          cx = cxInit;
+        } else {
+          cx += xGap;
+        }
 
-        <circle id="sl" cx="370" cy="200" r="22" fill={gray} />
-        <circle id="sl1" cx="370" cy="200" r="18" fill="url(#lockImage)" />
+        const nodeObjProps = {
+          id: `node-${node.id}`,
+          cx: cx,
+          cy: cy,
+          r: 22,
+          fill: gray,
+          ...(node.isCurrent && { r: 35, fill: lightOrange, }),
+          ...(node.isComplete && { fill: darkOrange, }),
+        }
 
-        <circle id="dot" cx="0" cy="0" r="2" stroke="green" strokeWidth={strokeWidth} fill="red" />
 
-        <path 
-          id='curve' 
-          style={{ stroke: 'url(#currentPathPattern)' }} 
-          fill='transparent' 
-          strokeWidth='4' 
-          strokeDasharray="13,10"
-          d='M35,50 Q40,100 150,50 T365,170' 
-        />
+        let curve = '';
+        if (idx > 0) {
+          const radius = 20;
+          const p1x = nodesRef.current[idx - 1].cx;
+          const p1y = nodesRef.current[idx - 1].cy;
+          const p2x = cx;
+          const p2y = cy;
 
-        {nodesArr.map(node => {
+          // mid-point of line:
+          const mpx = (p2x + p1x + radius) * 0.6;
+          const mpy = (p2y + p1y + radius) * 0.5;
 
-          return (<>
-            
-          </>);
-        })}
-      </svg>
-    </>
+          // angle of perpendicular to line:
+          const theta = Math.atan2(p2y - p1y, p2x - p1x) - Math.PI / 2;
+
+          // distance of control point from mid-point of line:
+          const offset = 60;
+
+          // location of control point:
+          const c1x = mpx + offset * Math.cos(theta);
+          const c1y = mpy + offset * Math.sin(theta);
+          // const dotElement = document.getElementById("dot");
+          // dotElement.setAttribute("cx", c1x);
+          // dotElement.setAttribute("cy", c1y);
+
+          const pathOffset = radius * 1.45;
+
+          // construct the command to draw a quadratic curve
+          // curve = "M" + p1x + " " + p1y + " Q " + c1x + " " + c1y + " " + p2x + " " + p2y;
+          curve = "M" + (p1x + pathOffset) + " " + p1y + " Q " + c1x + " " + c1y + " " + p2x + " " + (p2y - pathOffset);
+        }
+
+        nodesRef.current.push({ cx, cy });
+
+        const pathProps = {
+          id: `path-${node.id}`,
+          style: { stroke: "url(#currentPathPattern)" },
+          fill: "transparent",
+          strokeWidth: "4",
+          strokeDasharray: "13,10",
+          d: curve,
+        }
+
+        return (<>
+          <circle {...nodeObjProps} />
+          {node.isCurrent && <circle {...nodeObjProps} r="22" fill={darkOrange} />}
+          {node.isComplete && <circle {...nodeObjProps} r="18" fill="url(#okImage)" />}
+
+          {!node?.isComplete && !node?.isCurrent &&
+            <circle {...nodeObjProps} r="18" fill="url(#lockImage)" />
+          }
+
+          <path {...pathProps} />
+        </>);
+      })}
+    </svg>
   )
 }
 
